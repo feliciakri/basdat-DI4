9327463c0471518c14b6f3f9e65f3a40423fbe02
Felicia <feliciakrismanta@hotmail.com>
2017-05-11 23:29:48 +0700

    Created navbar skeleton, added logo, updated dbcon with pdo

--- dbconnect.php
+++ dbconnect.php
@@ -7,8 +7,8 @@
           $dbhost = 'localhost';
           $dbname='d04';
 
-          $conn = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass");
-          //$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+          //$conn = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass");
+          $conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
           return $conn;
       }catch (PDOException $e) {
           echo "Error : " . $e->getMessage() . "<br/>";
