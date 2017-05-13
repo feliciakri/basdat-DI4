6be6edc94c86e3d7ae5432eecbc9cbd3190c9bc6
safitridifa <safitrikaluza@gmail.com>
2017-05-13 13:50:18 +0700

    add laporan di file gue

--- zaza/registration.php
+++ zaza/registration.php
@@ -19,7 +19,7 @@
             $checkResult = pg_query($checkEmail);
 
             if(pg_num_rows($checkResult) > 0) {
-                echo '<script language="javascript">alert("Email sudah ada")</script>';
+                echo '<script language="javascript">alert("Alamat email ini sudah terdaftar, harap ganti dengan alamat email lain")</script>';
             } else {
                 $sql = "INSERT into pengguna(email, password, nama, jenis_kelamin, tgl_lahir, no_telp, alamat) values ('$email', '$password', '$nama', '$jenis_kelamin', '$tgl_lahir', '$no_telp', '$alamat')";    
                 if($result = pg_query($conn, $sql)) {
@@ -52,22 +52,12 @@
   <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
-    <script>
-        function validatePassword() {
-            var a = document.forms["form-regis"]["password"].value;
-            var b = document.forms["form-regis"]["re-password"].value;
-            if(!(a == b)) {
-                alert("Password salah");
-                return false;
-            }
-            return true;
-        }
-    </script>
     <div class="text-center">
         <div class="jumbotron">
             <h2>Sudahkah Anda #keren hari ini?</h2>
-            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#daftarModal">Register</button>
-            <button type="submit" class="btn btn-info" data-toggle="modal" data-target="#loginModal">Log in</button>
+            <h5>Masuk untuk melakukan transaksi bersama <strong>TOKOKEREN</strong> dengan menekan tombol di bawah ini!</h5>
+            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#daftarModal" data-placement="bottom" title="Untuk mendapatkan akun">Register</button>
+            <button type="submit" class="btn btn-info" data-toggle="modal" data-target="#loginModal" title="Untuk kembali keren">Log in</button>
             <br><br>
             <h5>#KerenAdalahHakSegalaBangsa</h5>
         </div>
@@ -78,16 +68,17 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title" id="daftarModalLabel">Form Pendaftaran Pengguna</h4>
+                    <h6>Mohon mengisi form sesuai dengan ketentuan. Tidak boleh ada field yang kosong. </h6>
                 </div>
             <div class="modal-body">
                 <form name="form-regis" action="registration.php" method="POST">
                     <div class="form-group">
                         <label for="email">E-mail</label>
-                        <input type="text" class="form-control" id="insert-email" name="email" placeholder="E-mail Anda" required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" title="Masukkan email dengan format alamat@domain.com atau alamat@domain.ac.id">
+                        <input type="text" class="form-control" id="insert-email" name="email" placeholder="Alamat email yang valid memiliki format alamat@contoh.com" required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" title="Masukkan email dengan format alamat@contoh.com atau alamat@mail.ac.id">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="insert-password" name="password" placeholder="Password Anda" required pattern=".{6,}" title="Password minimal terdiri atas 6 karakter">
+                        <input type="password" class="form-control" id="insert-password" name="password" placeholder="Password minimal terdiri atas 6 karakter" required pattern=".{6,}" title="Password minimal terdiri atas 6 karakter">
                     </div>
                     <div class="form-group">
                         <label for="re-password">Ulangi Password</label>
@@ -95,22 +86,23 @@
                     </div>
                     <div class="form-group">
                         <label for="nama">Nama Lengkap</label>
-                        <input type="text" class="form-control" id="insert-nama" name="nama" placeholder="Masukkan nama" required>
+                        <input type="text" class="form-control" id="insert-nama" name="nama" placeholder="Masukkan nama lengkap Anda" required>
                     </div>
                     <div class="form-group">
                         <label for="jenis_kelamin">Jenis Kelamin</label><br>
-                        <select classs="form-control" id="insert-jenis-kelamin" name="jenis_kelamin" required>
+                        <select class="form-control" id="insert-jenis-kelamin" name="jenis_kelamin" required>
+                            <option value="">Pilih satu</option>
                             <option value="L">Laki-laki</option>
                             <option value="P">Perempuan</option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="tgl_lahir">Tanggal Lahir</label>
-                        <input type="text" class="form-control" id="insert-alamat" name="tgl_lahir" placeholder="Masukkan tanggal lahir dengan format dd/mm/yyyy" required pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](\d\d|[0-9][0-9]))" title="dd/mm/yyyy">
+                        <input type="text" class="form-control" id="insert-alamat" name="tgl_lahir" placeholder="Tanggal lahir yang valid memiliki format dd/mm/yyyy" required pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](\d\d|[0-9][0-9]))" title="Tanggal lahir yang valid memiliki format dd/mm/yyyy">
                     </div>
                     <div class="form-group">
                         <label for="no_telp">Nomor Telepon</label>
-                        <input type="text" class="form-control" id="insert-no-telp" name="no_telp" placeholder="Nomor telepon yang dapat dihubungi" required pattern="(08)(\d{10})" title="Masukkan 12 digit angka diawali 08">
+                        <input type="text" class="form-control" id="insert-no-telp" name="no_telp" placeholder="Nomor telepon yang dapat dihubungi memiliki maksimal 12 digit" required pattern="(08)(\d{10})" title="Masukkan 12 digit angka diawali 08">
                     </div>
                     <div class="form-group">
                         <label for="alamat">Alamat</label>
@@ -147,6 +139,22 @@
             </div>
         </div>  
     </div>
+    <script type="text/javascript">
+        function validatePassword() {
+            var a = document.forms["form-regis"]["password"].value;
+            var b = document.forms["form-regis"]["re-password"].value;
+            if(!(a == b)) {
+                alert("Password harus sama");
+                return false;
+            }
+            return true;
+        }
+    </script>
+    <script type="text/javascript">
+        $(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js.js"></script>
